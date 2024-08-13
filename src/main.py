46137a106837fe1b742e5cc2608160e1f43f1cd4
David <88691455+david-0123@users.noreply.github.com>
2024-08-13 20:37:55 +0100

    Comment functions

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
 import random as rnd
 
+# Generates the initial game state with 'dead' cells weighted more, i.e. 'dead' cells should be expected more often
 def random_state(width, height):
     board = [[rnd.choices(population=[' ', chr(0x25A0)], weights=[0.8,0.2], k=1)[0] for x in range(width)] for y in range(height)]
     return board
 
+# Displays the current game state on the terminal with the correct format
+#  ͟͟͟͟͟͟͟͟͟͟
+# │■   ■     │
+# │   ■    ■ │
+# │   ■      │
+# │     ■ ■ ■│
+# │         ■│
+#  ͞͞͞͞͞͞͞͞͞͞
 def render(board):
     print(" "+chr(0x035F)*10)
     for i in range(len(board)):
